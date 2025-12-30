1a93d59a7164dc212a4a9b2f132b18cfaf69b4bd
navpreet-pineswift <navpreet@pineswift.com>
2025-12-31 01:25:35 +0530

    Fix TypeScript errors, add nanoid, update README for Supabase

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"QrERDHxp9Dpc0QqPoQQbU/W74I0lmDmac2bsrXKmuWg=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
